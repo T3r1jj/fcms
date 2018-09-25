96c32a884ef03031d1a2983940fba9739c4d71b1
Damian Terlecki <t3r1jj@gmail.com>
2018-09-26 01:38:53 +0200

    Initial commit started with Spring Initializr

---
+++ src/test/java/io/github/t3r1jj/fcms/FcmsApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.t3r1jj.fcms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FcmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

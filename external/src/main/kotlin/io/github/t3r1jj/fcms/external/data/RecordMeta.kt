ea085c113afacf70b59407405adab5fb95a43b11
Damian Terlecki <t3r1jj@gmail.com>
2018-11-04 18:30:28 +0100

    Externalize RecordMeta id

--- external/src/main/kotlin/io/github/t3r1jj/fcms/external/data/RecordMeta.kt
+++ external/src/main/kotlin/io/github/t3r1jj/fcms/external/data/RecordMeta.kt
@@ -1,5 +1,5 @@
 package io.github.t3r1jj.fcms.external.data
 
 data class RecordMeta(val name: String, val path: String, val size: Long) {
-    internal var id: String? = null
+    var id: String? = null
 }
